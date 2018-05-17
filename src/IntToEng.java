89c439d2cd632e2f40b593c0ee79394c8c1a0098
hinakokasahara <hinakokasahara@172.19.11.88>
2018-05-17 13:48:39 +0900

    1回目

---
+++ src/IntToEng.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class IntToEng {
+	public static void main(String[] args) {
+
+	    Scanner sc = new Scanner(System.in);
+	    int input = sc.nextInt();
+
+	    System.out.println(translateEng(input));
+
+	}
+	static String translateEng(int n) {
+	    if(n==1) return "one";
+	    if(n==2) return "two";
+	    if(n==3) return "three";
+	    if(n==4) return "four";
+	    if(n==5) return "five";
+	    if(n==6) return "six";
+	    if(n==7) return "seven";
+	    if(n==8) return "eight";
+	    if(n==9) return "nine";
+	    if(n==10) return "ten";
+	    else return "I don't know.";
+	}
+}
