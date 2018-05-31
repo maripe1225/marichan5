542c18921760acdbb85965769f8436a5bf80db20
mari <mari@172.19.15.224>
2018-05-31 10:44:17 +0900

    3回目

--- src/IntToEng.java
+++ src/IntToEng.java
@@ -10,27 +10,20 @@ public class IntToEng {
 
 	}
 	static String translateEng(int n) {
-	    if(n==1) return "one";
-	    else if(n==2) return "two";
-	    else if(n==3) return "three";
-	    else if(n==4) return "four";
-	    else if(n==5) return "five";
-	    else if(n==6) return "six";
-	    else if(n==7) return "seven";
-	    else if(n==8) return "eight";
-	    else if(n==9) return "nine";
-	    else if(n==10) return "ten";
-	    else if(n==11) return "eleven";
-	    else if(n==12) return "twelve";
-	    else if(n==13) return "thirteen";
-	    else if(n==14) return "fourteen";
-	    else if(n==15) return "fifteen";
-	    else if(n==16) return "sixteen";
-	    else if(n==17) return "seventeen";
-	    else if(n==18) return "eighteen";
-	    else if(n==19) return "nineteen";
-	    else if(n==20) return "twenty";
-	    
-	    else return "I don't know.";
+
+	    String[] num = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
+	    String[] num2 = {"eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+	    String[] num3 = {"", "", "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+	    if(n<=10) {
+	    	return num[n];
+	    } else if(n<20) {
+	    	return num2[n-11];
+	    } else if(n<100) {
+	    	String huta = num3[n/10];
+	    	String keta = num[n%10];
+	    	return huta+keta;
+	    } else {
+	    	return "I don't know.";
+	    }
 	}
 }
